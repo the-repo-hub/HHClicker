2b7b8fed9f561af036b849175788425a56870251
debian <debian@debian>
2023-04-08 22:17:34 +0300

    changed cookies overwriting

--- HH.py
+++ HH.py
@@ -15,12 +15,6 @@ class HHClicker(Firefox):
 
     def __init__(self):
         self.difference = 200  # разница между активным режимом и спящим (в сек)
-
-        try:
-            self.file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cookies.txt'), 'r+')
-        except FileNotFoundError:
-            exit("Can't find cookies.txt")
-
         self.cookies = self.get_cookies_from_file()
         super().__init__(service=Service(log_path='NUL'))
 
@@ -58,18 +52,22 @@ class HHClicker(Firefox):
         for c in self.cookies:
             self.add_cookie(c)
 
-    def get_cookies_from_file(self):
+    @staticmethod
+    def get_cookies_from_file():
         try:
-            result = ast.literal_eval(self.file.read())
-            if isinstance(result, list):
-                for c in result:
-                    if not isinstance(c, dict):
-                        exit('Not valid cookies!')
-                return result
-            else:
-                exit('Not valid cookies!')
+            with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cookies.txt')) as cookies:
+                result = ast.literal_eval(cookies.read())
+                if isinstance(result, list):
+                    for c in result:
+                        if not isinstance(c, dict):
+                            exit('Not valid cookies!')
+                    return result
+                else:
+                    exit('Not valid cookies!')
         except SyntaxError:
-            exit('Empty cookies.txt, run obtain_cookies.py first!')
+            exit('Empty cookies.py, run obtain_cookies.py first!')
+        except FileNotFoundError:
+            exit("Can't find cookies.txt")
 
     def auth_complete(self):
         if self.current_url == self.summary_url:
@@ -88,8 +86,9 @@ class HHClicker(Firefox):
             self.get(self.summary_url)
             if not self.auth_complete():
                 exit('Please, update cookies!')
-            self.file.write(self.get_cookies().__str__())
-            self.file.close()
+            with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cookies.txt'), 'w') as cookies:
+                cookies.write(self.get_cookies().__str__())
+
             buttons = self.find_elements(By.XPATH,'//button[@class="bloko-link"][@type="button"]['
                                                   '@data-qa="resume-update-button_actions"]')
             for button in buttons:
